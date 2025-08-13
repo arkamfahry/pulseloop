3feee15d7adb2dc04539e53f3cde23704ae37f09
pixelbend <92285161+pixelbend@users.noreply.github.com>
2025-08-13 08:16:20 +0530

    feat: change singin and singup form to use the figma design theme

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -3,66 +3,77 @@
 import { useAuthActions } from "@convex-dev/auth/react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { Navbar, Card, Label, TextInput, Checkbox, Button } from 'flowbite-react';
+import Link from 'next/link';
+import Image from "next/image";
 
 export default function SignIn() {
   const { signIn } = useAuthActions();
   const [error, setError] = useState<string | null>(null);
   const router = useRouter();
   return (
-    <div className="flex flex-col gap-8 w-96 mx-auto h-screen justify-center items-center">
-      <p>Welcome to PulseLoop</p>
-      <form
-        className="flex flex-col gap-2 w-full"
-        onSubmit={(e) => {
-          e.preventDefault();
-          const formData = new FormData(e.target as HTMLFormElement);
-          formData.set("flow", "signIn")
-          void signIn("password", formData)
-            .catch((error) => {
-              setError(error.message);
-            })
-            .then(() => {
-              router.push("/");
-            });
-        }}
-      >
-        <input
-          className="bg-background text-foreground rounded-md p-2 border-2 border-slate-200 dark:border-slate-800"
-          type="email"
-          name="email"
-          placeholder="Enter Email"
-          required
-        />
-        <input
-          className="bg-background text-foreground rounded-md p-2 border-2 border-slate-200 dark:border-slate-800"
-          type="password"
-          name="password"
-          placeholder="Enter Password"
-          required
-        />
-        <button
-          className="bg-foreground text-background rounded-md"
-          type="submit"
-        >
-          Sign In
-        </button>
-        <div className="flex flex-row gap-2">
-          <span>Don&apos;t have an account?</span>
-          <a
-            className="text-foreground underline hover:no-underline cursor-pointer"
-            href="/signup"
-          >
-            Sign Up
-          </a>
-        </div>
-        {error && (
-          <div className="bg-red-500/20 border-2 border-red-500/50 rounded-md p-2">
-            <p className="text-foreground font-mono text-xs">
-              Error signing in: {error}
-            </p>
+    <section className="bg-white dark:bg-gray-900">
+      <Navbar fluid rounded>
+        <Link href="/" className="flex items-center">
+          <Image src="/pulseloop.svg" width={36} height={36} className="mr-3 h-9 w-9" alt="PulseLoop" />
+          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">PulseLoop</span>
+        </Link>
+      </Navbar>
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <Card className="w-full bg-white rounded-2xl shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Welcome to PulseLoop
+            </h1>
+            <form 
+              className="space-y-4 md:space-y-6"
+              onSubmit={(e) => {
+                e.preventDefault();
+                const formData = new FormData(e.target as HTMLFormElement);
+                formData.set("flow", "signIn")
+                void signIn("password", formData)
+                  .catch((error) => {
+                    setError(error.message);
+                  })
+                  .then(() => {
+                    router.push("/");
+                  });
+              }}            
+            >
+              <div>
+                <div className="mb-2 block">
+                  <Label htmlFor="email">Email *</Label>
+                </div>
+                <TextInput id="email" name="email" placeholder="Enter your email" required type="email" />
+              </div>
+              <div>
+                <div className="mb-2 block">
+                  <Label htmlFor="password">Password *</Label>
+                </div>
+                <TextInput id="password" name="password" placeholder="••••••••" required type="password" />
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex items-start">
+                  <Checkbox id="remember" />
+                  <Label htmlFor="remember" className="ml-2">Remember me</Label>
+                </div>
+                <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Lost Password?</a>
+              </div>
+              <Button className="w-full" type="submit">Sign In</Button>
+              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                Don’t have an account? <a href="/signup" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign Up</a>
+              </p>
+              {error && (
+                <div className="bg-red-500/20 border-2 border-red-500/50 rounded-md p-2">
+                  <p className="text-foreground font-mono text-xs">
+                    Error signing in: {error}
+                  </p>
+                </div>
+              )}
+            </form>
           </div>
-        )}
-      </form>
-    </div>
+        </Card>
+      </div>
+    </section>
   );
 }
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -3,73 +3,84 @@
 import { useAuthActions } from "@convex-dev/auth/react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { Navbar, Card, Label, TextInput, Checkbox, Button } from 'flowbite-react';
+import Link from 'next/link';
+import Image from "next/image";
 
 export default function SignUp() {
   const { signIn } = useAuthActions();
   const [error, setError] = useState<string | null>(null);
   const router = useRouter();
   return (
-    <div className="flex flex-col gap-8 w-96 mx-auto h-screen justify-center items-center">
-      <p>Create a PulseLoop Account</p>
-      <form
-        className="flex flex-col gap-2 w-full"
+
+<section className="bg-white dark:bg-gray-900">
+<Navbar fluid rounded>
+  <Link href="/" className="flex items-center">
+    <Image src="/pulseloop.svg" width={36} height={36} className="mr-3 h-9 w-9" alt="PulseLoop" />
+    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">PulseLoop</span>
+  </Link>
+</Navbar>
+<div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+  <Card className="w-full bg-white rounded-2xl shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+      <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+        Welcome to PulseLoop
+      </h1>
+      <form 
+        className="space-y-4 md:space-y-6"
         onSubmit={(e) => {
           e.preventDefault();
           const formData = new FormData(e.target as HTMLFormElement);
-          formData.set("flow", "signUp");
+          formData.set("flow", "signUp")
           void signIn("password", formData)
-            .catch((error: Error) => {
+            .catch((error) => {
               setError(error.message);
             })
             .then(() => {
               router.push("/");
             });
-        }}
+        }}            
       >
-        <input
-          className="bg-background text-foreground rounded-md p-2 border-2 border-slate-200 dark:border-slate-800"
-          type="text"
-          name="name"
-          placeholder="Enter Name"
-          required
-        />
-        <input
-          className="bg-background text-foreground rounded-md p-2 border-2 border-slate-200 dark:border-slate-800"
-          type="email"
-          name="email"
-          placeholder="Enter Email"
-          required
-        />
-        <input
-          className="bg-background text-foreground rounded-md p-2 border-2 border-slate-200 dark:border-slate-800"
-          type="password"
-          name="password"
-          placeholder="Enter Password"
-          required
-        />
-        <button
-          className="bg-foreground text-background rounded-md"
-          type="submit"
-        >
-          Sign Up
-        </button>
-        <div className="flex flex-row gap-2">
-          <span>Have an account?</span>
-          <a
-            className="text-foreground underline hover:no-underline cursor-pointer"
-            href="/signin"
-          >
-            Sign In
-          </a>
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="name">Name *</Label>
+          </div>
+          <TextInput id="name" name="name" placeholder="Enter your name" required type="text" />
         </div>
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="email">Email *</Label>
+          </div>
+          <TextInput id="email" name="email" placeholder="Enter your email" required type="email" />
+        </div>
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="password">Password *</Label>
+          </div>
+          <TextInput id="password" name="password" placeholder="••••••••" required type="password" />
+        </div>
+        <div className="flex items-center justify-between">
+          <div className="flex items-start">
+            <Checkbox id="remember" />
+            <Label htmlFor="remember" className="ml-2">Remember me</Label>
+          </div>
+          <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Lost Password?</a>
+        </div>
+        <Button className="w-full" type="submit">Sign Up</Button>
+        <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+          Have an account? <a href="/signin" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign Up</a>
+        </p>
         {error && (
           <div className="bg-red-500/20 border-2 border-red-500/50 rounded-md p-2">
             <p className="text-foreground font-mono text-xs">
-              Error signing up: {error}
+              Error signing in: {error}
             </p>
           </div>
         )}
       </form>
     </div>
+  </Card>
+</div>
+</section>
   );
 }
