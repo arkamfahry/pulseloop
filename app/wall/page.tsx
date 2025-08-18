1987d761a0003268cac7b3d3c8548b3984637459
pixelbend <92285161+pixelbend@users.noreply.github.com>
2025-08-18 21:55:30 +0530

    feat: migrate project from NextJS to SvelteKit

--- app/wall/page.tsx
+++
@@ -1,5 +0,0 @@
-"use client";
-
-export default function Wall() {
-  return <div>Protected Wall</div>;
-}
--- src/convex/_generated/api.d.ts
+++ src/convex/_generated/api.d.ts
@@ -19,6 +19,7 @@ import type * as auth from "../auth.js";
 import type * as feedback from "../feedback.js";
 import type * as http from "../http.js";
 import type * as topics from "../topics.js";
+import type * as user from "../user.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -35,6 +36,7 @@ declare const fullApi: ApiFromModules<{
   feedback: typeof feedback;
   http: typeof http;
   topics: typeof topics;
+  user: typeof user;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
