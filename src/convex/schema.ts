88e3ffb9e6c8250f2161a2dadd4aace87d540427
pixelbend <92285161+pixelbend@users.noreply.github.com>
2025-09-16 20:02:31 +0530

    refactor: remove unused fields from users table in schema

--- src/convex/schema.ts
+++ src/convex/schema.ts
@@ -4,12 +4,7 @@ import { v } from 'convex/values';
 export default defineSchema({
 	users: defineTable({
 		name: v.optional(v.string()),
-		image: v.optional(v.string()),
 		email: v.optional(v.string()),
-		emailVerificationTime: v.optional(v.number()),
-		phone: v.optional(v.string()),
-		phoneVerificationTime: v.optional(v.number()),
-		isAnonymous: v.optional(v.boolean()),
 		role: v.optional(v.union(v.literal('user'), v.literal('admin')))
 	}).index('email', ['email']),
 
