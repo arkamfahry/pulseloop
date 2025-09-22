0c8e60ddb94b6bfb04fb6880d58d67f4b7cad693
pixelbend <92285161+pixelbend@users.noreply.github.com>
2025-09-22 16:22:12 +0530

    feat: add FeedBackApprovalCard component for managing feedback approval and deletion

--- src/routes/dashboard/approval/+page.svelte
+++ src/routes/dashboard/approval/+page.svelte
@@ -1,1 +1,50 @@
-<h1 class="text-2xl font-bold">Approval</h1>
+<script lang="ts">
+	import { api } from '$convex/_generated/api';
+	import SearchFilters from '$lib/dashboard/SearchFilters.svelte';
+	import { useQuery } from 'convex-svelte';
+	import { Spinner } from 'flowbite-svelte';
+	import FeedBackApprovalCard from '$lib/dashboard/FeedBackApprovalCard.svelte';
+
+	let filters = $state({
+		content: '',
+		sentiment: undefined as 'positive' | 'negative' | 'neutral' | undefined,
+		status: undefined as 'open' | 'noted' | undefined,
+		votes: undefined as 'asc' | 'desc' | undefined,
+		from: undefined as Date | undefined,
+		to: undefined as Date | undefined
+	});
+
+	const unpublishedFeedbackQuery = useQuery(api.feedback.listUnpublishedFeedback, () => ({
+		content: filters.content,
+		from: filters.from ? filters.from.getTime() : undefined,
+		to: filters.to ? filters.to.getTime() : undefined
+	}));
+</script>
+
+<main class="flex h-screen min-w-0 flex-col">
+	<section class="sticky top-0 z-10 flex justify-center bg-white">
+		<SearchFilters type="simple" bind:filters />
+	</section>
+
+	<section class="flex min-h-0 flex-1 flex-col items-center gap-4 px-1 pt-4">
+		{#if unpublishedFeedbackQuery.isLoading}
+			<div class="flex h-full items-center justify-center">
+				<Spinner />
+			</div>
+		{:else if unpublishedFeedbackQuery.error}
+			<p>Error: {unpublishedFeedbackQuery.error.message}</p>
+		{:else if unpublishedFeedbackQuery.data}
+			{#each unpublishedFeedbackQuery.data as feedback}
+				<FeedBackApprovalCard
+					id={feedback._id}
+					userName={feedback.user?.name ?? 'Anonymous'}
+					date={feedback.createdAt}
+					content={feedback.content}
+					votes={feedback.votes}
+				/>
+			{/each}
+		{:else}
+			<p class="text-gray-500">No feedback found.</p>
+		{/if}
+	</section>
+</main>
