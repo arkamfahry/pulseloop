00aa7c86fadf77712f2d580ebda34e4030ec3a85
pixelbend <92285161+pixelbend@users.noreply.github.com>
2025-09-22 16:21:30 +0530

    feat: enhance SearchFilters component to conditionally render sentiment and status buttons based on type

--- src/lib/dashboard/SearchFilters.svelte
+++ src/lib/dashboard/SearchFilters.svelte
@@ -14,9 +14,10 @@
 
 	interface Props {
 		filters: Filters;
+		type: 'full' | 'simple';
 	}
 
-	let { filters = $bindable() }: Props = $props();
+	let { filters = $bindable(), type = 'full' }: Props = $props();
 
 	let searchInput = $state(filters.content);
 	let debounceTimer: NodeJS.Timeout;
@@ -69,51 +70,53 @@
 		/>
 		<div class="flex w-full flex-row flex-wrap items-start gap-1">
 			<div class="flex min-w-[180px] flex-1 flex-col gap-2">
-				<div>
-					<div class="mb-2 font-semibold">Sentiment</div>
-					<div class="flex gap-2">
-						<Button
-							color="red"
-							outline={filters.sentiment !== 'negative'}
-							size="xs"
-							class="p-1"
-							onclick={() => updateSentiment('negative')}>Negative</Button
-						>
-						<Button
-							color="blue"
-							outline={filters.sentiment !== 'neutral'}
-							size="xs"
-							class="p-1"
-							onclick={() => updateSentiment('neutral')}>Neutral</Button
-						>
-						<Button
-							color="green"
-							outline={filters.sentiment !== 'positive'}
-							size="xs"
-							class="p-1"
-							onclick={() => updateSentiment('positive')}>Positive</Button
-						>
+				{#if type === 'full'}
+					<div>
+						<div class="mb-2 font-semibold">Sentiment</div>
+						<div class="flex gap-2">
+							<Button
+								color="red"
+								outline={filters.sentiment !== 'negative'}
+								size="xs"
+								class="p-1"
+								onclick={() => updateSentiment('negative')}>Negative</Button
+							>
+							<Button
+								color="blue"
+								outline={filters.sentiment !== 'neutral'}
+								size="xs"
+								class="p-1"
+								onclick={() => updateSentiment('neutral')}>Neutral</Button
+							>
+							<Button
+								color="green"
+								outline={filters.sentiment !== 'positive'}
+								size="xs"
+								class="p-1"
+								onclick={() => updateSentiment('positive')}>Positive</Button
+							>
+						</div>
 					</div>
-				</div>
-				<div>
-					<div class="mb-2 font-semibold">Status</div>
-					<div class="flex gap-2">
-						<Button
-							color="red"
-							outline={filters.status !== 'open'}
-							size="xs"
-							class="p-1"
-							onclick={() => updateStatus('open')}>Open</Button
-						>
-						<Button
-							color="green"
-							outline={filters.status !== 'noted'}
-							size="xs"
-							class="p-1"
-							onclick={() => updateStatus('noted')}>Noted</Button
-						>
+					<div>
+						<div class="mb-2 font-semibold">Status</div>
+						<div class="flex gap-2">
+							<Button
+								color="red"
+								outline={filters.status !== 'open'}
+								size="xs"
+								class="p-1"
+								onclick={() => updateStatus('open')}>Open</Button
+							>
+							<Button
+								color="green"
+								outline={filters.status !== 'noted'}
+								size="xs"
+								class="p-1"
+								onclick={() => updateStatus('noted')}>Noted</Button
+							>
+						</div>
 					</div>
-				</div>
+				{/if}
 			</div>
 			<div class="flex min-w-[220px] flex-col items-end justify-end gap-2 sm:w-auto">
 				<div class="flex w-full flex-col gap-2 sm:flex-row sm:gap-3">
@@ -136,18 +139,26 @@
 						/>
 					</div>
 				</div>
-				<div class="mt-2 flex w-full flex-col gap-2 sm:w-auto sm:flex-row">
-					<Button size="xs" color="light" class="w-full sm:w-auto" onclick={() => updateVotes()}>
-						{#if filters.votes === 'asc'}
-							<CaretDownOutline /> Vote
-						{:else}
-							<CaretUpOutline /> Vote
-						{/if}
-					</Button>
-					<Button size="xs" color="light" class="w-full sm:w-auto" onclick={() => clearFilters()}>
-						<CloseOutline /> Clear
-					</Button>
-				</div>
+				{#if type === 'full'}
+					<div class="mt-2 flex w-full flex-col gap-2 sm:w-auto sm:flex-row">
+						<Button size="xs" color="light" class="w-full sm:w-auto" onclick={() => updateVotes()}>
+							{#if filters.votes === 'asc'}
+								<CaretDownOutline /> Vote
+							{:else}
+								<CaretUpOutline /> Vote
+							{/if}
+						</Button>
+						<Button size="xs" color="light" class="w-full sm:w-auto" onclick={() => clearFilters()}>
+							<CloseOutline /> Clear
+						</Button>
+					</div>
+				{:else}
+					<div class="mt-2 flex w-full flex-col gap-2 sm:w-auto sm:flex-row">
+						<Button size="xs" color="light" class="w-full sm:w-auto" onclick={() => clearFilters()}>
+							<CloseOutline /> Clear
+						</Button>
+					</div>
+				{/if}
 			</div>
 		</div>
 	</div>
